79f229fec9052460bca4e45e4f88af00e4b8aa14
Jan von der Weyden <jvdw@gmx.net>
2017-02-15 18:06:35 +0100

    GridEditor Buttons

--- PidisWorld/Assets/Editor/GridDesignerEditor.cs
+++ PidisWorld/Assets/Editor/GridDesignerEditor.cs
@@ -1,15 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class GridDesignerEditor : EditorWindow
+
+[CustomEditor(typeof(Grid))]
+public class GridDesignerEditor : Editor
 {
     public GridTile[,] MainGrid;
 
 
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Grid grid = (Grid) target;
+
+        for (int x = (int) grid.SizeX - 1; x >= 0; x--)
+        {
+            EditorGUILayout.BeginHorizontal();
+            for (int y = 0; y < (int)grid.SizeY; y++)
+            {
+                if (GUILayout.Button(String.Format("{0} | {1}", x, y)))
+                {
+                    Debug.Log(String.Format("{0} | {1}", x, y));
+                }
+            }
+            EditorGUILayout.EndHorizontal();
+        }
 
 
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Create Grid"))
+        {
 
-
+        }
+    }
 }
