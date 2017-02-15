ba8fda5c6a5be8e6bf01aa0374dc12fea6046820
Jannick <demotw@gmx.de>
2017-02-15 19:44:39 +0100

    Added PlayerResources

---
+++ PidisWorld/Assets/Scripts/Gameplay/PlayerResources.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerResources : MonoBehaviour
+{
+    public static PlayerResources s_instance;
+    public static CultureInfo s_cultureInfo;
+
+    [SerializeField] private int m_iStartCredits;
+    [SerializeField] private int m_iStartWorkers;
+
+    [SerializeField] private ResourceCounter m_resourceCredits;
+    [SerializeField] private ResourceCounter m_resourceWorkers;
+
+    private int m_iCredits;
+    private int m_iWorkers;
+
+    private void Awake()
+    {
+        s_cultureInfo = CultureInfo.CreateSpecificCulture("de-DE"); ;
+        if (s_instance != null)
+        {
+            Debug.LogErrorFormat("There is already an instance of type {0}. Destroying this object.", GetType().Name);
+            Destroy(gameObject);
+        }
+        else
+        {
+            s_instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        InitializeResources();
+    }
+
+    private void InitializeResources()
+    {
+        m_iCredits = m_iStartCredits;
+        m_resourceCredits.SetValue(m_iCredits);
+        m_iWorkers = m_iStartWorkers;
+        m_resourceWorkers.SetValue(m_iWorkers);
+    }
+
+    public void AddCredits(int _iValue)
+    {
+        m_iCredits += _iValue;
+        m_resourceCredits.SetValue(m_iCredits);
+    }
+
+    public void AddWorkers(int _iValue)
+    {
+        m_iWorkers += _iValue;
+        m_resourceWorkers.SetValue(m_iWorkers);
+    }
+}
---
+++ PidisWorld/Assets/Scripts/Gameplay/ResourceCounter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceCounter : MonoBehaviour
+{
+    [SerializeField] private string m_strToStringVariant;
+    [SerializeField] private bool m_bValuePrefix = true;
+    [SerializeField] private bool m_bClampToPositive = false;
+    [SerializeField] private Color m_colPositive = Color.green;
+    [SerializeField] private Color m_colNegative = Color.red;
+
+    private Text m_txtCounter;
+
+    private void Awake()
+    {
+        m_txtCounter = GetComponent<Text>();
+        if (m_txtCounter == null)
+        {
+            Debug.LogErrorFormat("Missing Text Component on ResourceCounter {0}.", name);
+        }
+    }
+
+    public void SetValue(int _iValue)
+    {
+        if (m_bClampToPositive)
+        {
+            _iValue = Mathf.FloorToInt(Mathf.Clamp(_iValue, 0, Mathf.Infinity));
+        }
+        string strValuePrefix = "";
+        if (m_bValuePrefix && _iValue >= 0)
+        {
+            strValuePrefix = "+";
+        }
+        string strValue = string.Format("{0}{1}", strValuePrefix, _iValue.ToString(m_strToStringVariant, PlayerResources.s_cultureInfo));
+        m_txtCounter.text = strValue;
+        m_txtCounter.color = _iValue >= 0 ? m_colPositive : m_colNegative;
+    }
+}
