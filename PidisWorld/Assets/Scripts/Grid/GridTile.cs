3cf59416afa9916fc0a07a7660671727972c6412
Jan von der Weyden <jvdw@gmx.net>
2017-02-15 13:25:31 +0100

    Basic Grid Structure

---
+++ PidisWorld/Assets/Scripts/Grid/GridTile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTile : MonoBehaviour
+{
+    public Position Position;
+    public GridObject ContainedObject;
+
+    public bool IsOccupied()
+    {
+        return ContainedObject != null;
+    }
+
+}
+
+
+public class Position
+{
+    public int x;
+    public int y;
+
+    public Position(int X, int Y)
+    {
+        x = X;
+        y = Y;
+    }
+}
