146888cbab10ec44e1741acc5459ae0edd5e063d
Jannick <demotw@gmx.de>
2017-02-15 18:33:09 +0100

    Added WorkerMenu

---
+++ PidisWorld/Assets/Scripts/UI/WorkerMenu/WorkerCountGrid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorkerCountGrid : MonoBehaviour
+{
+    [SerializeField] private GameObject p_goNumberPrefab;
+    [SerializeField] private Sprite[] m_arNumberSprites;
+
+    [ContextMenu("Populate Grid")]
+    private void PopulateGrid()
+    {
+        List<GameObject> liChildren = new List<GameObject>();
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.activeInHierarchy)
+            {
+                liChildren.Add(child.gameObject);
+            }
+        }
+        foreach (GameObject child in liChildren)
+        {
+            DestroyImmediate(child);
+        }
+
+        foreach (Sprite numberSprite in m_arNumberSprites)
+        {
+            GameObject goNumber = Instantiate(p_goNumberPrefab, transform);
+            goNumber.SetActive(true);
+            goNumber.transform.GetComponentInChildren<Image>().sprite = numberSprite;
+        }
+    }
+}
